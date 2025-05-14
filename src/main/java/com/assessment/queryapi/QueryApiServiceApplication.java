2e7c72b85ddb4b7be56c4d4883a9dad97b09940a
Philip Narteh <Phinart98@gmail.com>
2025-05-14 22:03:06 +0000

    initial commit: starter project for query api service

---
+++ src/main/java/com/assessment/queryapi/QueryApiServiceApplication.java
@@ -0,0 +1,14 @@
+package com.assessment.queryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryApiServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(QueryApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/queryapi/QueryApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.queryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
